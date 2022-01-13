bd595b04a3baeb9e6c954ee232d93b72521ca809
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-01-13 09:49:39 +0000

    Add subnet for postgres

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,5 +1,11 @@
+data "azurerm_subnet" "postgres" {
+  name                 = "core-infra-subnet-0-${var.env}"
+  resource_group_name  = "core-infra-${var.env}"
+  virtual_network_name = "core-infra-vnet-${var.env}"
+}
+
 module "db" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=postgresql_tf"
   product            = var.product
   component          = var.component
   location           = var.location_db
@@ -7,6 +13,7 @@ module "db" {
   postgresql_user    = "et_msg_handler"
   database_name      = "et_msg_handler"
   postgresql_version = "11"
+  subnet_id          = data.azurerm_subnet.postgres.id
   common_tags        = local.tags
   subscription       = var.subscription
 }
