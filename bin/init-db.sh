0877d920ef24c00e5b0ad884e6824499f6ecdbba
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-01-21 11:41:12 +0000

    Remove jcenter

--- bin/init-db.sh
+++ bin/init-db.sh
@@ -3,12 +3,12 @@
 
 set -e
 
-if [ -z "$DB_URL" ] || [ -z "$ET_MSG_HANDLER_DB_PASSWORD" ]; then
-  echo "ERROR: Missing environment variables. Set value for 'DB_URL' and 'ET_MSG_HANDLER_DB_PASSWORD'."
+if [ -z "$DB_URL" ] || [ -z "ET_MSG_HANDLER_POSTGRES_PASSWORD" ]; then
+  echo "ERROR: Missing environment variables. Set value for 'DB_URL' and 'ET_MSG_HANDLER_POSTGRES_PASSWORD'."
   exit 1
 fi
 
-psql ${DB_URL} -v ON_ERROR_STOP=1 --username postgres --set USERNAME=et_msg_handler --set PASSWORD=${ET_MSG_HANDLER_DB_PASSWORD} <<-EOSQL
+psql ${DB_URL} -v ON_ERROR_STOP=1 --username postgres --set USERNAME=et_msg_handler --set PASSWORD=${ET_MSG_HANDLER_POSTGRES_PASSWORD} <<-EOSQL
   CREATE USER :USERNAME WITH PASSWORD ':PASSWORD';
   CREATE DATABASE et_msg_handler
     WITH OWNER = :USERNAME
