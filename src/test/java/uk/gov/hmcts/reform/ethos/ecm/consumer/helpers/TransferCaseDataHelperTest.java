8db649fd335da98cca5188ecd1946c7ba2ffe6c5
hsjhita <harpreet.jhita@justice.gov.uk>
2022-06-08 17:04:42 +0100

    Update TransferCaseDataHelperTest.java

--- src/test/java/uk/gov/hmcts/reform/ethos/ecm/consumer/helpers/TransferCaseDataHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/ethos/ecm/consumer/helpers/TransferCaseDataHelperTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.ethos.ecm.consumer.helpers;
 import org.junit.Test;
 import uk.gov.hmcts.ecm.common.model.ccd.CaseData;
 import uk.gov.hmcts.ecm.common.model.helper.TribunalOffice;
+import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
+import uk.gov.hmcts.et.common.model.bulk.types.DynamicValueType;
 import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.JurCodesTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.RespondentSumTypeItem;
@@ -23,16 +25,21 @@ public class TransferCaseDataHelperTest {
         var etCaseData = createEtCaseData();
         ecmCaseData = TransferCaseDataHelper.copyCaseData(etCaseData, ecmCaseData, "caseId", "ccdGatewayBsaeUrl",
                                                           "postionTypeCT", "Accepted", "Test");
+        assertEquals(TribunalOffice.GLASGOW.getOfficeName(), ecmCaseData.getManagingOffice());
+        assertEquals(TribunalOffice.GLASGOW.getOfficeName(), ecmCaseData.getAllocatedOffice());
+        assertEquals("Test", ecmCaseData.getFileLocationGlasgow());
         assertEquals(1, ecmCaseData.getJurCodesCollection().size());
         assertEquals("Test", ecmCaseData.getJurCodesCollection().get(0).getValue().getJuridictionCodesList());
         assertEquals(1, ecmCaseData.getRespondentCollection().size());
         assertEquals(1, ecmCaseData.getDocumentCollection().size());
+        assertEquals("Test", ecmCaseData.getClerkResponsible());
     }
 
     private uk.gov.hmcts.et.common.model.ccd.CaseData createEtCaseData() {
         var caseData = new uk.gov.hmcts.et.common.model.ccd.CaseData();
-        caseData.setManagingOffice(TribunalOffice.MANCHESTER.getOfficeName());
-
+        caseData.setManagingOffice(TribunalOffice.GLASGOW.getOfficeName());
+        caseData.setAllocatedOffice(TribunalOffice.GLASGOW.getOfficeName());
+        caseData.setFileLocationGlasgow(DynamicFixedListType.of(DynamicValueType.create("Test", "Test")));
         var jurCodesType = new JurCodesType();
         jurCodesType.setJuridictionCodesList("Test");
         jurCodesType.setJudgmentOutcome("Test");
@@ -53,7 +60,9 @@ public class TransferCaseDataHelperTest {
         documentTypeItem.setValue(documentType);
         caseData.setDocumentCollection(List.of(documentTypeItem));
 
+        caseData.setClerkResponsible(DynamicFixedListType.of(DynamicValueType.create("Test", "Test")));
+
         return caseData;
     }
-    
+
 }
