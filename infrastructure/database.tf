7f10e042776c55d0b395e0699775de0afc6ffb3a
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-03-27 10:22:17 +0000

    Remove v11 DB

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -3,17 +3,3 @@ data "azurerm_subnet" "postgres" {
   resource_group_name  = "core-infra-${var.env}"
   virtual_network_name = "core-infra-vnet-${var.env}"
 }
-
-module "db" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=postgresql_tf"
-  product            = var.product
-  component          = var.component
-  location           = var.location_db
-  env                = var.env
-  postgresql_user    = "et_msg_handler"
-  database_name      = "et_msg_handler"
-  postgresql_version = "11"
-  subnet_id          = data.azurerm_subnet.postgres.id
-  common_tags        = local.tags
-  subscription       = var.subscription
-}
