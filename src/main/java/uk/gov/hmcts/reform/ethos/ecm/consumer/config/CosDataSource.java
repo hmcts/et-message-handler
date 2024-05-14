12af2eaad656b03b8e7defd7549e7272997402be
Jack Reeve <jack@reevies.tech>
2024-05-14 10:24:43 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/config/CosDataSource.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/config/CosDataSource.java
@@ -1,15 +1,15 @@
 package uk.gov.hmcts.reform.ethos.ecm.consumer.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class CosDataSource {
-  @ConfigurationProperties("etcos.spring.datasource")
-  public DataSource etcos() {
-    return DataSourceBuilder.create().build();
-  }
+    @ConfigurationProperties("etcos.spring.datasource")
+    public DataSource etcos() {
+        return DataSourceBuilder.create().build();
+    }
 }
