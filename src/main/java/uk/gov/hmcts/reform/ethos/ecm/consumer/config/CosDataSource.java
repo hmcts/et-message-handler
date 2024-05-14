ea0c0365a1df491e52182b17b7324986262e30c5
Jack Reeve <jack@reevies.tech>
2024-05-14 11:20:15 +0100

    more bean names

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/config/CosDataSource.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/config/CosDataSource.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.ethos.ecm.consumer.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -10,6 +11,7 @@ import javax.sql.DataSource;
 @Configuration
 public class CosDataSource {
     @Qualifier("etcos")
+    @Bean(name = "etcos")
     @ConfigurationProperties("etcos.spring.datasource")
     public DataSource cosDataSource() {
         return DataSourceBuilder.create().build();
