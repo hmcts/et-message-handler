5444231999c27424b452f19cb45029c3be6688d3
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2026-03-09 08:27:46 +0000

    Add Azure Key Vault module and resource group configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,3 +34,19 @@ data "azurerm_user_assigned_identity" "et-identity" {
   name                = "${var.product}-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
 }
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "et_msg_handler_s2s_key" {
+  name         = "microservicekey-et-msg-handler"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "et_msg_handler_s2s_secret" {
+  name         = "et-msg-handler-s2s-secret"
+  value        = data.azurerm_key_vault_secret.et_msg_handler_s2s_key.value
+  key_vault_id = module.key-vault.key_vault_id
+}
