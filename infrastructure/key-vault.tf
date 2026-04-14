657950c8f93408d0847c07fddc10ede520f0065a
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2026-04-14 15:28:59 +0100

    TF Changes (#791)
    * Add Azure Key Vault module and resource group configuration
    
    * Add Azure Key Vault module and resource group configuration
    
    * Fix missing newline at end of main.tf
    
    Add missing newline at the end of main.tf file.
    
    * Update Tomcat and Netty versions in build.gradle
    
    * Update Netty version to 4.2.12.Final and bump et-shared dependency to 1.0.81
    
    * Add suppression for CVE-2026-23907 in suppressions.xml
    
    * Revert Key Vault secret configuration in main.tf
    
    * Revert Key Vault secret configuration in main.tf
    
    * Revert Key Vault module and resource group count logic for AAT environment
    
    * Fix resource group name reference in Key Vault configuration
    
    * Add moved configuration for Key Vault module and resource group
    
    * Update Tomcat embedded version to 9.0.117

---
+++ infrastructure/key-vault.tf
@@ -0,0 +1,13 @@
+module "key-vault" {
+  count                       = var.env == "aat" ? 0 : 1
+  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                        = "${var.product}-${var.component}-${var.env}"
+  product                     = var.product
+  env                         = var.env
+  tenant_id                   = var.tenant_id
+  object_id                   = var.jenkins_AAD_objectId
+  resource_group_name         = azurerm_resource_group.rg[0].name
+  product_group_name          = "DTS Employment Tribunals"
+  common_tags                 = local.tags
+  managed_identity_object_ids = [data.azurerm_user_assigned_identity.et-identity.principal_id]
+}
