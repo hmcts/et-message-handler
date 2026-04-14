d0a54ce4b2c335cd91a3eca9adf26c6134df3d95
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2026-04-14 13:45:56 +0100

    Fix resource group name reference in Key Vault configuration

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -6,7 +6,7 @@ module "key-vault" {
   env                         = var.env
   tenant_id                   = var.tenant_id
   object_id                   = var.jenkins_AAD_objectId
-  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = azurerm_resource_group.rg[0].name
   product_group_name          = "DTS Employment Tribunals"
   common_tags                 = local.tags
   managed_identity_object_ids = [data.azurerm_user_assigned_identity.et-identity.principal_id]
